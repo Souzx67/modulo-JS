a54d71235e8cde27b185df0b80b881bac5c9f22c
souzx <miguelsm2505@gmail.com>
2026-09-22 15:47:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let carro = "Skyline"
+let CarroMaisEspecifico = carro + "R34"
+let Placa = "FD67S69"
+const nome = "Miguel"   
+let idade = 16
+let cidade = "Londrina"
+const nomeCompleto = nome + " de Souza Matos"
+let hobby = "Estudar e Cozinhar"
+
+console.log(carro)
+console.log("Hello, world")
+console.log(`O meu nome é ${nome} e eu tenho ${idade} Anos de idade`)
+console.log(`Alguns Hobbys Meus São ${hobby}`)
+console.log(`Meu carro é o ${carro} mais especificamente o ${CarroMaisEspecifico} e a placa dele é ${Placa}`)
+console.log(`meu nome completo é ${nomeCompleto}`)
